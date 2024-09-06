b83cfca6a1f39c5e22d68640ab5d693ecab5856d
delcroip <patrick.delcroix@swisstph.ch>
2024-09-06 10:43:53 +0200

    try to force migration

--- contribution/migrations/0008_alter_psql_created_date.py
+++ contribution/migrations/0008_alter_psql_created_date.py
@@ -3,10 +3,6 @@ from django.conf import settings
 
 from datetime import datetime
 
-def is_postgresql(apps, schema_editor):
-    return schema_editor.connection.vendor == 'postgresql'
-
-
 class Migration(migrations.Migration):
     dependencies = [
         ('contribution', '0007_add_missing_fields_not_mssql'),
@@ -15,7 +11,7 @@ class Migration(migrations.Migration):
             migrations.AlterField(
                 model_name='premium',
                 name='created_date',
-                field=models.DateTimeField(db_column='CreatedDate', default=datetime.now()),
-            ) if is_postgresql else migrations.RunPython(lambda x, y: None)
+                field=models.DateTimeField(db_column='CreatedDate', default=datetime.now),
+            ) 
     ]
         
